1bb6f5249500c32a3a968d68b53b15b7fadff572
ElyriaG <ElyriaG@users.noreply.github.com>
2019-04-05 18:59:23 -0500

    random goal number,  repeating crystal numbers

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+
+var randomNumbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+// var randomNumberTwo = ""
+// var randomNumberThree = ""
+// var randomNumberFour = ""
+var wins = 0
+var losses = 0
+var playerScore = 0
+var goalNumber = ""
+var playerChoice = ""
+
+
+// Crystal Numbers
+var numberOne = Math.floor(Math.random() * randomNumbers.length + 1);
+$("#one").on("click", function() {
+    console.log(numberOne);
+})
+
+var numberTwo = Math.floor(Math.random() * randomNumbers.length + 1);
+$("#two").on("click", function() {
+    console.log(numberTwo);
+})
+
+var numberThree = Math.floor(Math.random() * randomNumbers.length + 1);
+$("#three").on("click", function() {
+    console.log(numberThree);
+})
+
+var numberFour = Math.floor(Math.random() * randomNumbers.length + 1);
+$("#four").on("click", function() {
+    console.log(numberFour);
+})
+
+
+
+
+
+
+// Random Goal Number
+for (var i = 0; i < 121; i++) {
+    var goalNumber = Math.floor(Math.random() * 100 + 9); 
+    }
+    $("#goal").text(goalNumber);
