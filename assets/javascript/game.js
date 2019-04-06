a6dca57216d2b5576c877bda4cb1602f967a23ac
ElyriaG <ElyriaG@users.noreply.github.com>
2019-04-05 20:32:50 -0500

    numbers are adding to player score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,5 @@
 
 var randomNumbers = [1,2,3,4,5,6,7,8,9,10,11,12]
-// var randomNumberTwo = ""
-// var randomNumberThree = ""
-// var randomNumberFour = ""
 var wins = 0
 var losses = 0
 var playerScore = 0
@@ -12,34 +9,33 @@ var playerChoice = ""
 
 // Crystal Numbers
 // $(document).ready(function() {
-// var numberOne = Math.floor(Math.random() * randomNumbers.length + 1);
-// $("#one").on("click", function() {
-//     console.log(numberOne);
-//     var playerScore = playerScore + $("#score").html(numberOne);
-// })
-// })
-var playerScore = []
-var numberTwo = Math.floor(Math.random() * randomNumbers.length + 1);
-$("#two").on("click", function() {
-    // $("#score").html(numberTwo);
-    console.log(playerScore + numberTwo);
+var numberOne = Math.floor(Math.random() * randomNumbers.length + 1);
+$("#one").on("click", function() {
+    console.log(numberOne);
+    playerScore = playerScore + numberOne
+    $("#score").text(playerScore);
 })
 
-$("#score").text(playerScore);
+var numberTwo = Math.floor(Math.random() * randomNumbers.length + 1);
+$("#two").on("click", function() {
+    console.log(numberTwo);
+   playerScore = playerScore + numberTwo
+   $("#score").text(playerScore);
+})
 
+var numberThree = Math.floor(Math.random() * randomNumbers.length + 1);
+$("#three").on("click", function() {
+    console.log(numberThree);
+    playerScore = playerScore + numberThree
+    $("#score").text(playerScore);
+})
 
-
-// var numberThree = Math.floor(Math.random() * randomNumbers.length + 1);
-// $("#three").on("click", function() {
-//     console.log(numberThree);
-//     var playerScore = playerScore + $("#score").html(numberThree);
-// })
-
-// var numberFour = Math.floor(Math.random() * randomNumbers.length + 1);
-// $("#four").on("click", function() {
-//     console.log(numberFour);
-//     var playerScore = playerScore + $("#score").html(numberFour);
-// })
+var numberFour = Math.floor(Math.random() * randomNumbers.length + 1);
+$("#four").on("click", function() {
+    console.log(numberFour);
+   playerScore = playerScore + numberFour
+   $("#score").text(playerScore);
+})
 
 
 
