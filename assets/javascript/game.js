ad96d892a3cc1eed5e8139583a1a374df3528a3d
ElyriaG <ElyriaG@users.noreply.github.com>
2019-04-05 23:17:50 -0500

    wins and losses add up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,122 @@
 
-var randomNumbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+var randomNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 var wins = 0
 var losses = 0
 var playerScore = 0
 var goalNumber = 0
-var playerChoice = ""
-
-
-// Crystal Numbers
-// $(document).ready(function() {
-var numberOne = Math.floor(Math.random() * randomNumbers.length + 1);
-$("#one").on("click", function() {
-    console.log(numberOne);
-    playerScore = playerScore + numberOne
-    $("#score").text(playerScore);
-})
-
-var numberTwo = Math.floor(Math.random() * randomNumbers.length + 1);
-$("#two").on("click", function() {
-    console.log(numberTwo);
-   playerScore = playerScore + numberTwo
-   $("#score").text(playerScore);
-})
-
-var numberThree = Math.floor(Math.random() * randomNumbers.length + 1);
-$("#three").on("click", function() {
-    console.log(numberThree);
-    playerScore = playerScore + numberThree
-    $("#score").text(playerScore);
-})
-
-var numberFour = Math.floor(Math.random() * randomNumbers.length + 1);
-$("#four").on("click", function() {
-    console.log(numberFour);
-   playerScore = playerScore + numberFour
-   $("#score").text(playerScore);
-})
-
-
-
-
-
 
 
 // Random Goal Number
-for (var i = 0; i < 121; i++) {
-    var goalNumber = Math.floor(Math.random() * 100 + 9); 
+ // Creating Goal Number
+ var newGoalNumber = function () {
+    for (var i = 0; i < 121; i++) {
+        goalNumber = Math.floor(Math.random() * 100 + 9);
+        $("#goal").text(goalNumber);
     }
-    $("#goal").text(goalNumber);
+    
+    };
+
+newGoalNumber()
+
+
+// Crystal Numbers
+$(document).ready(function () {
+    var numberOne = Math.floor(Math.random() * 3 + 1);
+    $("#one").on("click", function () {
+        playerScore = playerScore + numberOne
+        $("#score").text(playerScore);
+
+        if (playerScore === goalNumber) {
+            $("#score").text("You Won!");
+            $("#wins").text(+1);
+            newGoalNumber ();
+        }
+        if (playerScore > goalNumber){
+            $("#score").text("You Lost!");
+            $("#losses").text(+1);
+        }
+        else {
+            console.log("You suck!")
+        }
+    });
+    console.log(numberOne);
+
+
+    var numberTwo = Math.floor(Math.random() * 3 + 4);
+    $("#two").on("click", function () {
+        playerScore = playerScore + numberTwo
+        $("#score").text(playerScore);
+
+        if (playerScore === goalNumber) {
+            $("#score").text("You Won!");
+            $("#wins").text(+1);
+            newGoalNumber ();
+        }
+        if (playerScore > goalNumber){
+            $("#score").text("You Lost!");
+            $("#losses").text(+1);
+        }
+        else {
+            console.log("You suck!")
+        }
+    });
+    console.log(numberTwo);
+
+
+    var numberThree = Math.floor(Math.random() * 3 + 7);
+    $("#three").on("click", function () {
+        playerScore = playerScore + numberThree
+        $("#score").text(playerScore);
+
+        if (playerScore === goalNumber) {
+            $("#score").text("You Won!");
+            $("#wins").text(+1);
+            newGoalNumber ();
+        }
+        if (playerScore > goalNumber){
+            $("#score").text("You Lost!");
+            $("#losses").text(+1);
+        }
+        else {
+            console.log("You suck!")
+        }
+    });
+    console.log(numberThree);
+
+
+    var numberFour = Math.floor(Math.random() * 3 + 10);
+    $("#four").on("click", function () {
+
+        playerScore = playerScore + numberFour
+        $("#score").text(playerScore);
+
+        if (playerScore === goalNumber) {
+            $("#score").text("You Won!");
+            $("#wins").text(+1);
+            newGoalNumber ();
+        }
+        if (playerScore > goalNumber){
+            $("#score").text("You Lost!");
+            $("#losses").text(+1);
+        }
+        else {
+            console.log("You suck!")
+        }
+    });
+    console.log(numberFour);
+
+
+   
+   
+    
+
+    
+
+})
+
+
+
+
+
+
+
